b3e3ce34166841422ffe08d5a0269f1dea8866a5
BiggerThings <nnsbiggerthings@gmail.com>
2025-04-26 09:59:09 +0200

    Made dashboard a protected route using redirect component for non-authenticated users

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,18 @@
-export default function DashboardRoute() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardRoute() {
+  const { getUser } = getKindeServerSession();
+    
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/register");
+  }
+  
   return (
     <div className="py-6">
       <h1>Hello from the dashboard page</h1>
     </div>
   )
 }
-
-
-///To Implement auth 
